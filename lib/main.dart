7e1b24345b60affa2d9a67db687eb050a46e0b36
@sachin.vishvakarma <sachin.vishvakarma@techinfini.com>
2026-03-16 18:24:31 +0530

    Updated Code

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
     super.key,
   });
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     ScreenUtil.init(context);
